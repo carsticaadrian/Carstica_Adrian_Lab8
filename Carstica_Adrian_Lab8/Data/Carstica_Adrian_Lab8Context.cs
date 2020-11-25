81603c112d0a40b8d5badac69413a7e5c06cc0ff
Carstica Adrian <carsticaadrian@gmail.com>
2020-11-25 15:39:49 +0200

    Add project files.

---
+++ Carstica_Adrian_Lab8/Data/Carstica_Adrian_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Carstica_Adrian_Lab8.Models;
+
+namespace Carstica_Adrian_Lab8.Data
+{
+    public class Carstica_Adrian_Lab8Context : DbContext
+    {
+        public Carstica_Adrian_Lab8Context (DbContextOptions<Carstica_Adrian_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Carstica_Adrian_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Carstica_Adrian_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Carstica_Adrian_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Carstica_Adrian_Lab8/Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Carstica_Adrian_Lab8.Data;
+using Carstica_Adrian_Lab8.Models;
+
+namespace Carstica_Adrian_Lab8.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Carstica_Adrian_Lab8.Data.Carstica_Adrian_Lab8Context _context;
+
+        public IndexModel(Carstica_Adrian_Lab8.Data.Carstica_Adrian_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
